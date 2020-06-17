2a41f246398b6a7bbae6a463c3fadb05a42c8957
wszgrcy <wszgrcy@gmail.com>
2020-06-17 08:14:10 +0000

    页面构建

--- src/app/set-background.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SetBackgroundDirective } from './set-background.directive';
-
-describe('SetBackgroundDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SetBackgroundDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/code/main.ts
+++
@@ -1,7 +0,0 @@
-import chroma from "chroma-js";
-import { main } from './class';
-
-let color = chroma('#F44336').gl()
-let glColor = { red: color[0], green: color[1], blue: color[2], alpha: color[3] || 1 }
-let list = main(color)
-console.log(list)
